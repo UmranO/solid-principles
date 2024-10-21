16b70277063137f0a26e5bac39b891305e9dbc91
Umran Ozman <umonal@yahoo.com>
2024-10-21 00:50:06 -0400

    X Class updated

--- src/com/cydeo/X.java
+++ src/com/cydeo/X.java
@@ -3,9 +3,24 @@ package com.cydeo;
 public class X {
     public static void main(String[] args) {
 
-        Z z= new Y();
-
-        z.method();
+        Z z= new Y();                      //Here Polymorphisim Z is the interface with an abstract method
+                                           //Y is a class that implements that interface Z
+        z.method();                        //We are calling in Class X the Interface's method from the object of
+                                           //class Y which is implementing Interface Z.
+                                           //Bu noktada source code dependency olarak ciplak gozle kodu takip etmek
+                                           //istedigimizde main module'den baslayip zinteface gidiyoruz ama z'de sadece
+                                           // abstract method var. Buradan sonrasinda implementation'in nerede oldugunu
+                                           //direkt goremiyoruz. Inter.Z'de boyle bir bilgi yok. Ama ClassY'de bu bilgi
+                                           //mevcut:
+                                                    /*
+                                                    public class Y implements Z {
+                                                    @Override
+                                                    public void method() {
+                                                                           }
+                                                     */
+                                           //Dolayisiyla burada tight coupling olmasin diye istedigimiz source code
+                                           //dependency'nin terse donme durumu gerceklesmis oldu. UOS Bilgi akisi terse
+                                           //dondu.
 
     }
 }
