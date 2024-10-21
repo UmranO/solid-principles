accb9d30b90897e047a987ff95ccb781672d0c95
Umran Ozman <umonal@yahoo.com>
2024-10-21 14:31:52 -0400

    User Class updated

--- src/com/cydeo/solid/singleResponsibility/bad/User.java
+++ src/com/cydeo/solid/singleResponsibility/bad/User.java
@@ -6,6 +6,11 @@ public class User {
         // Implementation for getting the API request.
         // Continue with the requested CRUD operation.
     }
+    public static boolean validate() {
+        boolean flag = false;
+        // Check validity of the API request.
+        return flag;
+    }
 
 
 
