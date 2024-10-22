f3b41aabff80be9137d7aa321a0754d0b959d08e
Umran Ozman <umonal@yahoo.com>
2024-10-21 20:48:49 -0400

    UserController Class updated

--- src/com/cydeo/solid/singleResponsibility/goodExample/UserController.java
+++ src/com/cydeo/solid/singleResponsibility/goodExample/UserController.java
@@ -1,4 +1,9 @@
 package com.cydeo.solid.singleResponsibility.goodExample;
 
 public class UserController {
+
+    public static void getRequest() {
+        // Get the request
+        // Send this request to validation and security part
+    }
 }
