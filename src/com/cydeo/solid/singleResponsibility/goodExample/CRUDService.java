8acfbb7396eb9f881be7e8dd20bad7e8f00c1117
Umran Ozman <umonal@yahoo.com>
2024-10-21 21:19:52 -0400

    CRUDService Class updated

--- src/com/cydeo/solid/singleResponsibility/goodExample/CRUDService.java
+++ src/com/cydeo/solid/singleResponsibility/goodExample/CRUDService.java
@@ -1,4 +1,12 @@
 package com.cydeo.solid.singleResponsibility.goodExample;
 
 public class CRUDService {
+    public static void create(){
+        //Create a user
+        //Send user to database related part
+    }
+    public static void read() {
+        // Read a user
+        // Send user to database related part
+    }
 }
