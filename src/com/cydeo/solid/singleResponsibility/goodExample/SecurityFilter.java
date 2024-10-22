5bdd5c847e01c6cd2f70d4bcb9656884c32ed5ce
Umran Ozman <umonal@yahoo.com>
2024-10-21 21:09:49 -0400

    SecurityFilter Class updated

--- src/com/cydeo/solid/singleResponsibility/goodExample/SecurityFilter.java
+++ src/com/cydeo/solid/singleResponsibility/goodExample/SecurityFilter.java
@@ -1,9 +1,12 @@
 package com.cydeo.solid.singleResponsibility.goodExample;
 
 public class SecurityFilter {
-
     public static boolean authenticate(){
         //Check authentication
+        return true;
+    }
+    public static boolean authorization(){
+        //Check authorization
         return true;
     }
 }
