7de7faf1798cd740abbce95a6baf78bed12ee2a3
Umran Ozman <umonal@yahoo.com>
2024-10-22 10:51:25 -0400

    Calculator class updated

--- src/main/java/com/cydeo/solid/openClosed/example2/bad/Calculator.java
+++ src/main/java/com/cydeo/solid/openClosed/example2/bad/Calculator.java
@@ -1,4 +1,10 @@
 package com.cydeo.solid.openClosed.example2.bad;
 
 public class Calculator {
+    public void calculate(Operation operation) {
+
+        int num1 = 10;
+        int num2 = 2;
+
+    }
 }
