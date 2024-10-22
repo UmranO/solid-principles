948fa6d1d7ed74059b47a28a59a0ba9b9e9db68f
Umran Ozman <umonal@yahoo.com>
2024-10-22 10:44:39 -0400

    UOTest class created

---
+++ src/main/java/com/cydeo/solid/openClosed/example1/good/UOTest.java
@@ -0,0 +1,14 @@
+package com.cydeo.solid.openClosed.example1.good;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class UOTest {
+    public static void main(String[] args) {
+        Circle c = new Circle(1.2);
+        Rectangle r = new Rectangle(2.0, 1.1);
+
+        List<Shape> l = Arrays.asList(c, r, new Triangle());
+
+    }
+}
