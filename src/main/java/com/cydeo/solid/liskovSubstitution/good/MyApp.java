db6f04f78339b31192b71f8662ef4d0a72680c77
Umran Ozman <umonal@yahoo.com>
2024-10-22 19:40:09 -0400

    MyApp Class updated

--- src/main/java/com/cydeo/solid/liskovSubstitution/good/MyApp.java
+++ src/main/java/com/cydeo/solid/liskovSubstitution/good/MyApp.java
@@ -3,5 +3,10 @@ package com.cydeo.solid.liskovSubstitution.good;
 public class MyApp {
     public static void main(String[] args) {
 
+        Rectangle rectangle = new Rectangle();
+        rectangle.setHeight(3);
+        rectangle.setWidth(5);
+
+       
     }
 }
