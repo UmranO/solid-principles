d22210e5ac73a073824af152d9b779acbc6e6f54
Umran Ozman <umonal@yahoo.com>
2024-10-22 19:34:14 -0400

    Square class updated

--- src/main/java/com/cydeo/solid/liskovSubstitution/bad/Square.java
+++ src/main/java/com/cydeo/solid/liskovSubstitution/bad/Square.java
@@ -1,4 +1,16 @@
 package com.cydeo.solid.liskovSubstitution.bad;
 
 public class Square extends Rectangle{
+    @Override                             //Since Square extends it can use the height & width(rectangle's fields & ()s)
+    public void setHeight(int height) {   //But it overridden the setters here.
+        this.height = height;
+        this.width = height;
+    }
+
+    @Override
+    public void setWidth(int width) {
+        this.width = width;
+        this.height = width;
+    }
+
 }
