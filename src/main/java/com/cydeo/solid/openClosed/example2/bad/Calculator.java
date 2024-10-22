74f10e072fbce1b51a168868c6fad9a65dab6c32
Umran Ozman <umonal@yahoo.com>
2024-10-22 10:54:56 -0400

    Calculator class updated

--- src/main/java/com/cydeo/solid/openClosed/example2/bad/Calculator.java
+++ src/main/java/com/cydeo/solid/openClosed/example2/bad/Calculator.java
@@ -6,5 +6,17 @@ public class Calculator {
         int num1 = 10;
         int num2 = 2;
 
+        if(operation.getType()==OperationType.ADDITION){
+            System.out.println( num1+num2);
+        }
+        if(operation.getType()==OperationType.SUBTRACTION){
+            System.out.println( num1-num2);
+        }
+        if(operation.getType()==OperationType.MULTIPLICATION){
+            System.out.println(num1 * num2);
+        }
+        if(operation.getType()==OperationType.DIVISION){
+            System.out.println(num1/num2);
+        }
     }
 }
