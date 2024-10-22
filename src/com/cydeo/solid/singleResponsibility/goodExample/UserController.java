35196822ac7bcc7064a02669e85e59be5fe32b48
Umran Ozman <umonal@yahoo.com>
2024-10-21 20:52:01 -0400

    UserController Class updated

--- src/com/cydeo/solid/singleResponsibility/goodExample/UserController.java
+++ src/com/cydeo/solid/singleResponsibility/goodExample/UserController.java
@@ -1,6 +1,10 @@
 package com.cydeo.solid.singleResponsibility.goodExample;
-
 public class UserController {
+
+//This is a good example for Single Responsibility Principle
+//We don't put all the methods we need in this class.
+//This class has only 3()s which are related with getting the request and response
+
 
     public static void getRequest() {
         // Get the request
