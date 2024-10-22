a3879194b0e50ada3e005271132a68fe04c46e8a
Umran Ozman <umonal@yahoo.com>
2024-10-21 21:02:11 -0400

    Manager Class updated

--- src/com/cydeo/solid/singleResponsibility/goodExample/Manager.java
+++ src/com/cydeo/solid/singleResponsibility/goodExample/Manager.java
@@ -1,4 +1,9 @@
 package com.cydeo.solid.singleResponsibility.goodExample;
 
 public class Manager {
+
+    public static boolean validate(){
+        //check validation
+        return true;
+    }
 }
