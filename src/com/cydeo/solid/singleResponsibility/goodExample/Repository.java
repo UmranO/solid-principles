26a8ea31e1f47d7771e9e1b9d2e1225b22630d42
Umran Ozman <umonal@yahoo.com>
2024-10-21 21:28:36 -0400

    Repository Class updated

--- src/com/cydeo/solid/singleResponsibility/goodExample/Repository.java
+++ src/com/cydeo/solid/singleResponsibility/goodExample/Repository.java
@@ -1,5 +1,9 @@
 package com.cydeo.solid.singleResponsibility.goodExample;
 
 public class Repository {
+    public static void create() {
+        // Connect to database
+        // Create user in database
+    }
 
 }
