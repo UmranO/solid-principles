ce4990851f952f0a05bf9b877aec0fc339377c3f
Umran Ozman <umonal@yahoo.com>
2024-10-23 12:50:04 -0400

    UserReader class updated

--- src/main/java/com/cydeo/solid/dependencyInversion/badExample/UserReader.java
+++ src/main/java/com/cydeo/solid/dependencyInversion/badExample/UserReader.java
@@ -1,4 +1,14 @@
 package com.cydeo.solid.dependencyInversion.badExample;
 
 public class UserReader {
+    private XMLReader xmlReader;
+
+    public UserReader(XMLReader xmlReader) {
+        this.xmlReader = xmlReader;
+    }
+
+    public String getUsername() {
+        return xmlReader.getUsername();
+    }
+
 }
