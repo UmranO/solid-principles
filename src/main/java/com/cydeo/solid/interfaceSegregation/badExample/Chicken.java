f89f2b20a7d14e689d06f8672297d04f130e1fdb
Umran Ozman <umonal@yahoo.com>
2024-10-23 08:56:01 -0400

    Chicken class updated

--- src/main/java/com/cydeo/solid/interfaceSegregation/badExample/Chicken.java
+++ src/main/java/com/cydeo/solid/interfaceSegregation/badExample/Chicken.java
@@ -1,4 +1,20 @@
 package com.cydeo.solid.interfaceSegregation.badExample;
 
-public class Chicken {
+public class Chicken implements Bird {
+
+    @Override
+    public void eat() {
+        System.out.println("Eating.");
+    }
+
+    @Override
+    public void walk() {
+        System.out.println("Walking.");
+    }
+
+    @Override
+    public void fly() {                                    //Since Chicken can't fly it should NOT implement Bird
+        // Can not fly, so do nothing.
+    }
+
 }
