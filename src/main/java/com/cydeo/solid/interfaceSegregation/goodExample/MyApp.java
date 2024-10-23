6e9c9ab526dfadb3ed7d4e010cae6b67159e92f0
Umran Ozman <umonal@yahoo.com>
2024-10-23 10:39:29 -0400

    MyApp class updated

--- src/main/java/com/cydeo/solid/interfaceSegregation/goodExample/MyApp.java
+++ src/main/java/com/cydeo/solid/interfaceSegregation/goodExample/MyApp.java
@@ -1,4 +1,15 @@
 package com.cydeo.solid.interfaceSegregation.goodExample;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyApp {
+    public static void main(String[] args) {
+
+    List<CanFly> birds= new ArrayList<>();
+        birds.add(new Eagle());
+        birds.add(new Owl());
+  //    birds.add(new Chicken());     // Compiler says that chicken can not fly.
+
+
+    }
 }
