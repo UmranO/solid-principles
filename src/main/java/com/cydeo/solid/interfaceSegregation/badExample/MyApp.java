befaecc6ddc1969e3b957b24d71010bae4e28d44
Umran Ozman <umonal@yahoo.com>
2024-10-23 10:32:01 -0400

    MyApp class updated

--- src/main/java/com/cydeo/solid/interfaceSegregation/badExample/MyApp.java
+++ src/main/java/com/cydeo/solid/interfaceSegregation/badExample/MyApp.java
@@ -1,7 +1,11 @@
 package com.cydeo.solid.interfaceSegregation.badExample;
+import java.util.List;
+import java.util.ArrayList;
 
 public class MyApp {
     public static void main(String[] args) {
+
+        List<Bird> birds=new ArrayList<>();
 
     }
 }
