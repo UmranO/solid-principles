aee72386e42df9d76d3d87bb1f6f59cce327e591
Umran Ozman <umonal@yahoo.com>
2024-10-23 08:54:03 -0400

    Owl class updated

--- src/main/java/com/cydeo/solid/interfaceSegregation/badExample/Owl.java
+++ src/main/java/com/cydeo/solid/interfaceSegregation/badExample/Owl.java
@@ -1,4 +1,18 @@
 package com.cydeo.solid.interfaceSegregation.badExample;
 
-public class Owl {
+public class Owl implements Bird {
+    @Override
+    public void eat() {
+        System.out.println("Eating.");
+    }
+
+    @Override
+    public void walk() {
+        System.out.println("Walking.");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("Flying");
+    }
 }
