9c41d7f041fb80427272e2c53c24139e1371a2e3
Umran Ozman <umonal@yahoo.com>
2024-10-23 10:42:55 -0400

    Eagle  class updated

--- src/main/java/com/cydeo/solid/interfaceSegregation/goodExample/Eagle.java
+++ src/main/java/com/cydeo/solid/interfaceSegregation/goodExample/Eagle.java
@@ -3,17 +3,17 @@ package com.cydeo.solid.interfaceSegregation.goodExample;
 public class Eagle implements CanEat, CanWalk, CanFly {        //Since Eagle is able to do all it implements all
     @Override
     public void eat() {
-        System.out.println("Eating.");
+        System.out.println("Eagle Eating.");
     }
 
     @Override
     public void fly() {
-        System.out.println("Flying.");
+        System.out.println("Eagle Flying.");
     }
 
     @Override
     public void walk() {
-        System.out.println("Walking.");
+        System.out.println("Eagle Walking.");
     }
 
 }
