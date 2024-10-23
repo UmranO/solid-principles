838949c475052701df04967112abbfc1c57c6cf6
Umran Ozman <umonal@yahoo.com>
2024-10-23 10:04:29 -0400

    Chicken class updated

--- src/main/java/com/cydeo/solid/interfaceSegregation/goodExample/Chicken.java
+++ src/main/java/com/cydeo/solid/interfaceSegregation/goodExample/Chicken.java
@@ -1,4 +1,12 @@
 package com.cydeo.solid.interfaceSegregation.goodExample;
 
-public class Chicken {
+public class Chicken implements CanEat, CanWalk{
+    @Override
+    public void eat() {
+        System.out.println("Eating");
+    }
+    @Override
+    public void walk() {
+        System.out.println("Walking");
+    }
 }
