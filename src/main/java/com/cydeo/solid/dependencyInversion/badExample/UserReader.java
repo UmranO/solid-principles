3dab62a0a1dc39d2efc165e03a46868ca27502fb
Umran Ozman <umonal@yahoo.com>
2024-10-25 11:54:13 -0400

    UserReader class updated

--- src/main/java/com/cydeo/solid/dependencyInversion/badExample/UserReader.java
+++ src/main/java/com/cydeo/solid/dependencyInversion/badExample/UserReader.java
@@ -5,6 +5,7 @@ public class UserReader {
     //private JSonReader jsonReader;
 
     public UserReader(XMLReader xmlReader) {     // We need to add jsonReader in Constructor too
+
         this.xmlReader = xmlReader;
     }
 
