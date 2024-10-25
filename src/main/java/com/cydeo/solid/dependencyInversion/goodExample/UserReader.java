9ac26998dc17e4268f0ba974e7d4cb7e7eac92be
Umran Ozman <umonal@yahoo.com>
2024-10-25 12:21:07 -0400

    UserReader class updated

--- src/main/java/com/cydeo/solid/dependencyInversion/goodExample/UserReader.java
+++ src/main/java/com/cydeo/solid/dependencyInversion/goodExample/UserReader.java
@@ -1,4 +1,7 @@
 package com.cydeo.solid.dependencyInversion.goodExample;
 
 public class UserReader {
+    private Reader reader;                      //Instead of makinfg UserReader CLass directly dependent on XMLReader
+                                                //Class we need to put an abstraction in between so put Reader reader
+                                                //as a field instead of private XMLReader xmlReader;
 }
