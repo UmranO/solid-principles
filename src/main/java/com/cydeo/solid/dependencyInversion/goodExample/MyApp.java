956b4aa20fc872fc0fe64881d797225950633345
Umran Ozman <umonal@yahoo.com>
2024-10-25 13:20:18 -0400

    MyApp class updated

--- src/main/java/com/cydeo/solid/dependencyInversion/goodExample/MyApp.java
+++ src/main/java/com/cydeo/solid/dependencyInversion/goodExample/MyApp.java
@@ -1,4 +1,16 @@
 package com.cydeo.solid.dependencyInversion.goodExample;
 
 public class MyApp {
+   // public static void main(String[] args) {
+   //     XMLReader xMLReader =new XMLReader();                Normalde bunlar websitesindeki forma end user'in girdigi
+   //     UserReader userReader=new UserReader(xMLReader);     bilgiden gelecek dolayisiyla tek tek create etmene gerek
+   //     userReader.getUsername();                            kalmayacak o zaman da main method yerine baska bir metod
+   // }                                                        olacak eg read()
+
+//Mesela:
+    public static void read(Reader reader){
+        UserReader userReader=new UserReader(reader);
+        userReader.getUsername();
+
+    }
 }
