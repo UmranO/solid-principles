ced8e2aeb3cadae5c544f44ba12bae3e303e3a9b
Umran Ozman <umonal@yahoo.com>
2024-10-25 11:08:56 -0400

    UserReader class updated

--- src/main/java/com/cydeo/solid/dependencyInversion/badExample/UserReader.java
+++ src/main/java/com/cydeo/solid/dependencyInversion/badExample/UserReader.java
@@ -2,6 +2,7 @@ package com.cydeo.solid.dependencyInversion.badExample;
 
 public class UserReader {
     private XMLReader xmlReader;                 // What happens if we decide to use JSON format instead of XML format?
+    //private JSonReader jsonReader;
 
     public UserReader(XMLReader xmlReader) {     // We need to add jsonReader in Constructor too
         this.xmlReader = xmlReader;
@@ -10,7 +11,7 @@ public class UserReader {
     public String getUsername() {                // public String getUsernameFromXML()
         return xmlReader.getUsername();
     }
-//    public String getUsernameFromJSON() {     //We should add a method to get the other format
+//    public String getUsernameFromJSON() {      //We should add a method to get the other format
 //        return jsonReader.getUsername();
 //    }
 
