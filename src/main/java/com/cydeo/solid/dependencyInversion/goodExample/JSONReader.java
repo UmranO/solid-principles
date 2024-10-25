e037864667f0ba9755ca496e86a98f05ec735495
Umran Ozman <umonal@yahoo.com>
2024-10-25 12:48:53 -0400

    JSONReader class updated

--- src/main/java/com/cydeo/solid/dependencyInversion/goodExample/JSONReader.java
+++ src/main/java/com/cydeo/solid/dependencyInversion/goodExample/JSONReader.java
@@ -1,4 +1,8 @@
 package com.cydeo.solid.dependencyInversion.goodExample;
 
-public class JSONReader {
+public class JSONReader implements Reader{
+    @Override
+    public String getUserName() {
+        return null;
+    }
 }
